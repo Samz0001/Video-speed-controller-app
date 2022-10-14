548e5554b69229be2379bd95ae6efebe7cc07b2d
SAM <sishere23@gmail.com>
2022-10-14 21:16:27 +0530

    VIDEO CONTROLLER

---
+++ app.js
@@ -0,0 +1,17 @@
+const video  = document.querySelector(".video")
+const speed = document.querySelector(".speed")
+const bar = document.querySelector(".speed-bar")
+
+
+speed.addEventListener("mousemove", function(e) {
+    console.log(e)
+    const x = e.pageX - speed.offsetLeft;
+    const percent = x / speed.offsetWidth;
+const min = 0.1;
+const max = 5;
+const width = Math.round(percent * 100) + "%"
+const playBackRate = percent * (max -min) + min
+bar.style.width = width
+bar.textContent = playBackRate.toFixed(2) + "x"
+video.playbackRate = playBackRate;
+})  
